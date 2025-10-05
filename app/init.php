fa72936e4903d7c4c54209480e4694121c48e44c
JackyAkagami <jackyrahmatul02@gmail.com>
2025-10-05 15:32:30 +0700

    REVISI NOTE

---
+++ app/init.php
@@ -0,0 +1,10 @@
+<?php
+// Load config
+require_once 'config/config.php';
+
+// Load Core
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+require_once 'core/Database.php';
+
+// Bisa ditambah helper (jika ada)
---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/E-Commerce/public');
+
+// Database Config
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ecommerce_db');
